2b95ea91c008bcb87858f475ac14b601141e2ff7
Jeb.Wang <jeb.wannng@gmail.com>
2023-06-15 16:24:11 +0800

    temporary commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StarknetController } from './starknet/starknet.controller';
+import { StarknetService } from './starknet/starknet.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, StarknetController],
+  providers: [AppService, StarknetService],
 })
 export class AppModule {}
